ffa563fb1faad3076175c8f2f4cb2b8fa8f1c70e
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-02-22 20:41:59 +0530

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,11 @@
+firebaseConfig = {
+    apiKey: "AIzaSyA1Mf_6Hk0QkT6smXYtECgGLOPNtycF-4U",
+    authDomain: "children-book-6215b.firebaseapp.com",
+    databaseURL: "https://children-book-6215b-default-rtdb.firebaseio.com",
+    projectId: "children-book-6215b",
+    storageBucket: "children-book-6215b.appspot.com",
+    messagingSenderId: "998656061466",
+    appId: "1:998656061466:web:3460616093a099226f61a3",
+    measurementId: "G-CRX3695FYX"
+  };
+firebase.initializeApp(firebaseConfig);
---
+++ main.js
@@ -0,0 +1,19 @@
+firebaseConfig = {
+    apiKey: "AIzaSyA1Mf_6Hk0QkT6smXYtECgGLOPNtycF-4U",
+    authDomain: "children-book-6215b.firebaseapp.com",
+    databaseURL: "https://children-book-6215b-default-rtdb.firebaseio.com",
+    projectId: "children-book-6215b",
+    storageBucket: "children-book-6215b.appspot.com",
+    messagingSenderId: "998656061466",
+    appId: "1:998656061466:web:3460616093a099226f61a3",
+    measurementId: "G-CRX3695FYX"
+  };
+firebase.initializeApp(firebaseConfig);
+
+firstname = document.getElementById("first_name").value;
+lastname = document.getElementById("last_name").value;
+fullname = firstname + lastname;
+
+function signup() {
+    console.log(fullname);
+}
